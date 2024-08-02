8a901dd015ed993a06a4d0c238a6600e8864c189
Barry <124591126+Bharath-Nayakanti@users.noreply.github.com>
2024-08-02 10:21:49 +0530

    Delete Positive_numbers.py

--- Positive_numbers.py
+++
@@ -1,7 +0,0 @@
-numbers = input("Enter a list of numbers, separated by commas: ").split(",")
-numbers = [int(num) for num in numbers]
-positive_numbers = []
-for num in numbers:
-    if num > 0:
-        positive_numbers.append(num)
-print("Positive numbers From the given range are:", positive_numbers)
